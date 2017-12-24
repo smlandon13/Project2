04b2387dc25c25c368953cc5c7231f40d3916bcd
s.landon <smlandon13@me.com>
2017-12-23 23:10:01 -0700

    Commit

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function autoDate () {
+    var tDay = new Date();
+    var tMonth = tDay.getMonth()+1;
+    var tDate = tDay.getDate();
+    if ( tMonth < 10) tMonth = "0"+tMonth;
+    if ( tDate < 10) tDate = "0"+tDate;
+    document.getElementById("tDate").value = tMonth+"/"+tDate+"/"+tDay.getFullYear();
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            if (oldonload) {
+                oldonload();
+            }
+            func();
+        }
+    }
+}
+
+addLoadEvent(function() {
+    autoDate();
+});
